818f65eac4a31e0015e431e5129d62b13f631406
熊雨翔 <xiongyuxiang1996@163.com>
2019-08-07 17:57:55 +0800

    删除session存入

--- src/main/java/com/shiro/ServiceImpl/LoginServiceImpl.java
+++ src/main/java/com/shiro/ServiceImpl/LoginServiceImpl.java
@@ -8,7 +8,6 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Service;
 
@@ -60,8 +59,6 @@ public class LoginServiceImpl implements LoginService {
                 loginResult.setResult(msg);
             }
             else if(user.getState() == 1){
-                Session session = currentUser.getSession();
-                session.setAttribute("username", username);
                 loginResult.setLogin(true);
             }
             else if(user.getState() == 2){
